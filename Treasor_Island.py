c618e6f3f523eb8bdec0591077aa2fd0162b3fe9
AmarokRa <165519026+AmarokRa@users.noreply.github.com>
2024-06-07 11:17:31 +0200

    Add files via upload

---
+++ Treasor_Island.py
@@ -0,0 +1,61 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("To start the adventure, you have to cross the forest of Hen.")
+
+direction_forest = input("You run through the forest of Hen and after 30 minutes you arrive at the seaside. Choose your path !\n 'left' or 'right' ?").lower()
+
+if direction_forest == 'left':
+    direction_sea = input("You ran through the forest and arrived at the seaside. What is your decision ?\n 'pray' or 'swim' ?").lower()
+    if direction_sea == 'pray':
+        direction_castle = input("A spirit of the forest gives you the power to cross the sea. You arrive to the other side of the sea. You can see a big castle and you go towards it. You arrive in front of the  Windsor Castle ! The doors are open !\n 'enter' or 'knock'?").lower()
+        if direction_castle == 'enter':
+            choose_door = input("You enter the doors. You observe the entrance hall. There is nothing interesting. But you can see three doors in this room ! You can choose only one door !\n 'red', 'blue' , 'yellow' or 'another' ?").lower()
+            if choose_door == 'yellow':
+                fight_moster = input("Unlucky !\n Behind this door, there was a big monster ! Next to him, there was a chest. You fought the monster and, after a very long fight, you decide to use a special move. To defeat the monster, you can choose one special move.\n 'SunnyYellow', 'JungleGreen' or 'UltraViolet' ?").lower()
+                if fight_moster == 'SunnyYellow':
+                    open = input("A mini sun burns the monster. Congratulations, you have destroyed the monster ! You have access to the chest ! Do you want open the chest ?\n 'Y' or 'N'").lower()
+                    if open == 'Y':
+                        print("Congratulations, this is the end of your adventure !!! You open the chest and take the treasure.\n You win !")
+                    else:
+                        print("The bomb exploded !\n You are dead !\n Game Over ! ")
+                elif fight_moster == 'JungleGreen':
+                    print("A part of the Hen's strength unfolds in your body. You use this power and destroy the monster and the castle. Because of this power, everything is destroyed and the treasure too !\n You lose !\n Game Over ! ")
+                elif fight_moster == 'UltraViolet':
+                    print("This special move transforms you into a purple Power Ranger ! With the power of this costume, you kill the monster but the latter has a malediction which melts everything within a radius of 2km !\n You are dead !\n Game Over !")
+                else:
+                    print("Your strength leaves you because of the monster's curse which lasts 35 minutes. You end up shredded and dismembed by the monster !\n You are dead !\n Game Over ! ")
+
+            elif choose_door == 'red':
+                print("You open the door\n The fire of Hell devours your soul and your body !\n You are dead !\n Game Over !")
+            elif choose_door == 'blue':
+                print("You open the door\n The wolves smell your scent and attack you !\n You are dead !\n Game Over ! ")
+            else:
+                print("You wait. After 2 hours of waiting, you fall asleep to recover your strength. During the night, while you are asleep, the wandering souls of the castle possess your body and kill your soul !\n You are dead !\n Game Over !")
+        else:
+            print("A pack of carnivore monkeys heard you knocking and attack you !\n You are dead ! Game Over !")
+    else:
+        print("After 10 minnutes of swimming, sharks attack you !\n You are dead !\n Game Over ! ")
+else:
+    print("You chose the wrong path and the wandering souls of the forest eat your soul !\n You are dead !\n Game Over !")
